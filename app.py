abc9425adb52c14980e3663b6246a68da27e2180
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-11-15 22:50:24 +0530

    test.....

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     try:
-        return render_template("index.html")
+        return "<p>Hello, World!</p>"
+        # return render_template("index.html")
     except Exception as e:
         return str(e)
 
