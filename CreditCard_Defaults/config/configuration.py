8cb3bd5de224c44eafc72214284a438777e73a08
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-11-18 18:10:22 +0530

    configuration up..

--- CreditCard_Defaults/config/configuration.py
+++ CreditCard_Defaults/config/configuration.py
@@ -18,12 +18,12 @@ from CreditCard_Defaults.entity.config_entity import (
 class Configuration:
 
     def __init__(self, config_file_path: str = CONFIG_FILE_PATH,
-                 current_time_stmap: str = CURRENT_TIME_STAMP
+                 current_time_stamp: str = CURRENT_TIME_STAMP
                  ) -> None:
         try:
             self.config_info = read_yaml_file(file_path=config_file_path)
             self.training_pipeline_config = self.get_training_pipeline_config()
-            self.time_stamp = current_time_stmap
+            self.time_stamp = current_time_stamp
         except Exception as error:
             raise DefaultException(error, sys) from error
 
